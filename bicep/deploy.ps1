e49818bcc96c5a4a384b9b30c8ad4d3336b1f1a4
Daniel Fang <qkfang@hotmail.com>
2026-06-07 12:03:31 +1000

    refactor: update deployment group name and remove unused monitoring resources

--- bicep/deploy.ps1
+++ bicep/deploy.ps1
@@ -1,4 +1,4 @@
 
 az group create --name 'rg-playground-shared' --location 'australiaeast'
 
-az deployment group create --name 'playground-shared-deploy' --resource-group 'rg-playground-shared' --template-file './main.bicep' --parameters './main.bicepparam' --query 'properties.outputs' -o json | ConvertFrom-Json
+az deployment group create --name 'iac-deploy' --resource-group 'rg-playground-shared' --template-file './main.bicep' --parameters './main.bicepparam' --query 'properties.outputs' -o json | ConvertFrom-Json
